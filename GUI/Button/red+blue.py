f1578073ca5de089c580d3e59945d82bb39779d6
Legend971 <shauryyagupta09@gmail.com>
2024-03-06 14:23:31 +1300

    changed the values a bit

--- GUI/Button/red+blue.py
+++ GUI/Button/red+blue.py
@@ -7,9 +7,9 @@ root = tk.Tk()
 root.title("Color Changer")
 
 red_button = tk.Button(root, text="Red", command=lambda: change_color("red"))
-red_button.pack(side="left", padx=10)
+red_button.pack(side="left", padx=15)
 
 blue_button = tk.Button(root, text="Blue", command=lambda: change_color("blue"))
-blue_button.pack(side="right", padx=10)
+blue_button.pack(side="right", padx=15)
 
 root.mainloop()

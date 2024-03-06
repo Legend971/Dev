686bc94468564246a2aca552b0361e66405dc953
Legend971 <shauryyagupta09@gmail.com>
2024-03-06 14:19:32 +1300

    2 buttons, red and blue

---
+++ GUI/Button/red+blue.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+def change_color(color):
+    root.configure(bg=color)
+
+root = tk.Tk()
+root.title("Color Changer")
+
+red_button = tk.Button(root, text="Red", command=lambda: change_color("red"))
+red_button.pack(side="left", padx=10)
+
+blue_button = tk.Button(root, text="Blue", command=lambda: change_color("blue"))
+blue_button.pack(side="right", padx=10)
+
+root.mainloop()
